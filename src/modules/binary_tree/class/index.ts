41c44c03464cd0d5981fe5bedf70113aab207584
couvq <quentincouvelaire@gmail.com>
2025-02-24 12:27:22 +0100

    initial setup and a class for a binary tree node

---
+++ src/modules/binary_tree/class/index.ts
@@ -0,0 +1,10 @@
+class BinaryTreeNode<T> {
+  val: T;
+  left: BinaryTreeNode<T> | null;
+  right: BinaryTreeNode<T> | null;
+  constructor(val: T) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
