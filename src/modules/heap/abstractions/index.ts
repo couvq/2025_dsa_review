35d4ee36f3b894151e94f8160d44255cf6ae4a20
couvq <quentincouvelaire@gmail.com>
2025-04-18 08:40:00 -0400

    implemented min heap push

--- src/modules/heap/abstractions/index.ts
+++ src/modules/heap/abstractions/index.ts
@@ -1,6 +1,6 @@
 interface Heap<T> {
   push(val: T): void;
-  pop(): T;
+  pop(): T | undefined;
 }
 
 class MinHeap implements Heap<number> {
@@ -23,8 +23,37 @@ class MinHeap implements Heap<number> {
     }
   }
 
-  pop(): number {
-    throw new Error("Method not implemented.");
+  pop(): number | undefined {
+    if (this.#heapArray.length === 1)
+      throw new Error("Cannot pop off an empty heap.");
+    if (this.#heapArray.length === 2) return this.#heapArray.pop();
+
+    const returnVal = this.#heapArray[1];
+    // move last element to root
+    // @ts-ignore
+    this.#heapArray[1] = this.#heapArray.pop();
+    let i = 1;
+    // iterate while we have a left child
+    while(2 * i < this.#heapArray.length) {
+        if(2 * i + 1 < this.#heapArray.length) {
+            if(this.#heapArray[2 * i] < this.#heapArray[2 * i + 1]) {
+                this.#swap(2 * i, i)
+                i = 2 * i
+            } else {
+                this.#swap(2 * i + 1, i)
+                i = 2 * i + 1
+            }
+        } else {
+            if(this.#heapArray[2 * i] > this.#heapArray[i]) {
+                break;
+            } else {
+                this.#swap(2 * i, i)
+                i = 2 * i
+            }
+        }
+    }
+
+    return returnVal;
   }
 
   printValues(): void {
@@ -33,7 +62,7 @@ class MinHeap implements Heap<number> {
 
   #swap(i: number, j: number): void {
     const temp = this.#heapArray[i];
-    this.#heapArray[i] = j;
+    this.#heapArray[i] = this.#heapArray[j];
     this.#heapArray[j] = temp;
   }
 }
