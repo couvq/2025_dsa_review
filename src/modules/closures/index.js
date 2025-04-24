0d92c32f4f859ff8b1324d0195c3744e7634f93b
couvq <quentincouvelaire@gmail.com>
2025-04-24 09:00:23 -0400

    refactored to include a value closure to read count value so our event listener can decide which side effect to occur.

--- src/modules/closures/index.js
+++ src/modules/closures/index.js
@@ -1,19 +1,22 @@
 const createCounter = () => {
   let count = 0;
-  const display = document.getElementById("display");
-  display.innerText = count;
 
-  // closure with private access to the count & display variables
-  return () => {
-    count += 1;
-    display.innerText = count;
+  // returns closures with private access to the count variable
+  return {
+    value: () => count,
+    count: () => {
+      count += 1;
+    },
   };
 };
 
-const count = createCounter();
+const counter = createCounter();
 
 const countBtn = document.getElementById("count_btn");
+const display = document.getElementById("display");
+display.innerText = counter.value()
 
 countBtn?.addEventListener("click", () => {
-  count();
+  counter.count()
+  display.innerText = counter.value()
 });
